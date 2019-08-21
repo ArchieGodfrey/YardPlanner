d404f4dd846bbefa69dfb6b4a10b34045c65ce94
Archie <archiegod@gmail.com>
2019-08-21 15:24:48 +0100

    Temp data

---
+++ src/data.js
@@ -0,0 +1,101 @@
+// eslint-disable-next-line import/prefer-default-export
+export const data = {
+  Tewin: {
+    id: 1,
+    staff: [
+      {
+        id: 1,
+        name: 'staffOne',
+        phoneNumber: 123456789,
+      },
+      {
+        id: 2,
+        name: 'staffTwo',
+        phoneNumber: 123456789,
+      },
+    ],
+    members: [
+      {
+        id: 1,
+        name: 'memberOne',
+        phoneNumber: 123456789,
+        horses: [
+          {
+            id: 1,
+            name: 'horseOne',
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: 'memberTwo',
+        phoneNumber: 123456789,
+        horses: [
+          {
+            id: 2,
+            name: 'horseTwo',
+          },
+        ],
+      },
+    ],
+    horses: [
+      {
+        id: 1,
+        name: 'horseOne',
+      },
+      {
+        id: 2,
+        name: 'horseTwo',
+      },
+    ],
+  },
+  YardTwo: {
+    id: 1,
+    staff: [
+      {
+        id: 1,
+        name: 'staffOne',
+        phoneNumber: 123456789,
+      },
+      {
+        id: 2,
+        name: 'staffTwo',
+        phoneNumber: 123456789,
+      },
+    ],
+    members: [
+      {
+        id: 1,
+        name: 'memberOne',
+        phoneNumber: 123456789,
+        horses: [
+          {
+            id: 1,
+            name: 'horseOne',
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: 'memberTwo',
+        phoneNumber: 123456789,
+        horses: [
+          {
+            id: 2,
+            name: 'horseTwo',
+          },
+        ],
+      },
+    ],
+    horses: [
+      {
+        id: 1,
+        name: 'horseOne',
+      },
+      {
+        id: 2,
+        name: 'horseTwo',
+      },
+    ],
+  },
+};
